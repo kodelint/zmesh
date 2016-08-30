5554741f12c53177713eb8524c558589c78abd82
Satyajit Roy <git4sroy@github.com>
2016-08-30 10:33:36 -0700

    Adding couple of local alias and export files

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -7,6 +7,8 @@ alias be='bundle exec'
 # e.g. for silo in $silos; do be cap $silo smn deploy:upload_configs; be cap $silo smn deploy:restart; done;
 source /Users/satyajit.roy/.rvm/scripts/rvm
 source /usr/local/bin/virtualenvwrapper.sh
+source /Users/satyajit.roy/Dropbox/Personal/mygit/pexports.zsh
+source /Users/satyajit.roy/Dropbox/Personal/mygit/palias.zsh
 [[ $- = *i* ]] && source $HOME/.zmesh/zsh/liquidprompt/liquidprompt.plugin.zsh
 
 

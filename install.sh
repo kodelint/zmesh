5baefa47bedce3bb85eeb23cf92604e2ce3b078f
Satyajit Roy <kodelint@gmail.com>
2016-11-14 13:53:18 -0800

    Major typo in install script

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 if [ ! -d "$HOME/.zmesh" ]; then
     echo "Installing zmesh for the first time"
-    git clone --depth=1 https://github.com/git4sroy/zmesh.git $HOME/.zmesh
+    git clone --depth=1 https://github.com/kodelint/zmesh.git $HOME/.zmesh
     cd "$HOME/.zmesh"
     [ "$1" = "ask" ] && export ASK="true"
     rake install
